86d27c151ee5568049a42fef741a7c71993865fb
nelsonvilte <nelsonvilte@gmail.com>
2023-11-06 23:27:10 -0300

    agregado proyecto calculadora

---
+++ script.js
@@ -0,0 +1,28 @@
+// calculadora.js
+
+function calcular(numero) {
+    const operand1 = parseFloat(document.getElementById('operand1').value);
+    const operand2 = parseFloat(document.getElementById('operand2').value);
+    let result = 0;
+
+    switch (numero) {
+        case '+':
+            result = operand1 + operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        case '*':
+            result = operand1 * operand2;
+            break;
+        case '/':
+            if (operand2 !== 0) {
+                result = operand1 / operand2;
+            } else {
+                alert("No se puede dividir por cero.");
+            }
+            break;
+    }
+
+    document.getElementById('result').textContent = result;
+}
